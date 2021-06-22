05e1c07a89f7ee03ac26ce452c1ef03088adfc5f
IVBecy <kristofhracza@gmail.com>
2021-06-22 13:18:36 +0100

    Removed includes, switched to linux env.

--- socket.cpp
+++ socket.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <socket.h>
+#include <string>
+#include <sys/socket.h>
 using namespace std;
 
-// Socket class
-
+// Socket function
+// will be used to create all the sockets later on
+void createSocket(string host,int port){
+	socket(AF_INET, SOCK_STREAM, 0);
+};
 
 // MAIN
 int main(){
-  cout << "MAIN";
-};
+	createSocket("kristofhracza.com",443);
+	return 0;
+}
